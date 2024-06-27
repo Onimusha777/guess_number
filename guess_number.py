d8c6a68259f8243663e25d71bcd00e33edb34e5a
Сергей Мушенко <onimusha777@bk.ru>
2024-06-27 11:15:20 +0500

    add initial commit

---
+++ guess_number.py
@@ -0,0 +1,19 @@
+from random import randint
+
+number = randint(1,100)
+
+
+def main():
+    while True:
+        userNumber = int(input('Введите число: '))
+        if userNumber < number:
+            print('Число меньше')
+        elif userNumber > number:
+            print('Число больше')
+        else:
+            break
+
+    print('Отличная интуиция!')
+
+
+main()
